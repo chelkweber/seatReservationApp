32ba949012bbbba4f408505d773192a12592b4bd
Chelsea Weber <chelseaweber92@gmail.com>
2017-05-17 16:49:01 -0400

    clean up script

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-$(document).ready(function(){
-	//drop-down seating area
-	$('.seatPicker').click(function(){
-		$('#seating-area').toggle('slow');
-	});
-
-	$('#seating-area').click(function(){
-		$('#reservation-form').slideDown('slow');
-
-	})
-
+$(document).ready(function(){	
+	//create variables
 	var seats = [];
-
-	var seats2 = [];
-
-	var seat;
-
-	var selectedSeat;
-
-	var guestName;
-
-	var name;
-
-	//create function to push 24 seats, with props, into seats array
+	var seat = {};
+	var selectedSeat = {};
+	
+	//call functions and event handlers
+	addSeats();
+	$('.seat').click(selectSeat);
+	$('#submit-btn').click(reserveSeat);
+	$('#submit-btn').click(clearForm);
+	
+	//create seat objects and push, with props, into seats array
 	function addSeats() {
 		for (i=1; i<25; i++) {
 			seat = {
 				number: i,
+				status: 'a',
 				firstName: '',
 				lastName: '',
 				phoneNumber: '',
@@ -35,65 +25,59 @@ $(document).ready(function(){
 			seats.push(seat);
 		}
 	}
-
-	addSeats();
-
-	console.log(seats);
-	//change color of selected seat, assign selectedSeat value
-	$('.seat').click(function(e) {
-		guestName = $(this).attr("data-lastname", "test");
-
-		if ($(this).hasClass('selected')) {
-			$(this).removeClass('selected');
-			$(this).css('background-color', '#E5E7E6');
-		} else {
-			$(this).addClass('selected').css('background-color', '#646362');
-		}
-
-		selectedSeat = e.target.innerText;
-
-		
-	});
-
-
-	//function to add user info to selected seat
-	$('#submit-btn').click(function() {
+	
+	//select seat, change status prop in object, change class and color of div
+	function selectSeat(e){
+		e = event.target;
 		seats.forEach(function(seat) {
-			if (selectedSeat == seat.number) {
+			if (e.innerText == seat.number && seat.status==='a') {
+				seat.status = 's';
+			} else if (e.innerText == seat.number && seat.status==='s') {
+				seat.status = 'a';
+			}
+		})
+		if ($(this).hasClass('availible')) {
+			$(this).removeClass('availible').addClass('selected');
+			$(this).css('background-color', '#646362');
+		} else if ($(this).hasClass('selected')) { 
+			$(this).removeClass('selected').addClass('availible');
+			$(this).css('background-color', '#E5E7E6')
+		}
+		console.log(this);
+	}
+	
+	//reserve seat, change props of object, change class and text of div
+	function reserveSeat() {
+		seats.forEach(function(seat) {
+			if(seat.status === 's') {
+				seat.status = 'r';
 				seat.firstName = $('#first-name').val();
 				seat.lastName = $('#last-name').val();
 				seat.phoneNumber = $('#phone').val();
 				seat.email = $('#email').val();
 				seat.request = $('#request').val();
-				guestName.attr('data-lastname', seat.lastName)
 			}
-		})
-		if ($('div').hasClass('selected')) {
+		});
+		if ($('.seat').hasClass('selected')) {
 			$('.selected').removeClass('selected').addClass('reserved');
 			$('.reserved p').text('Reserved');
-		}
-
-//		$('.reserved').attr('data-lastname', 'test');
-//		console.log(data-lastname);
-		console.log(guestName);
-
-	});
+		};
+		console.log(seats);
+	};
 	
-	console.log(seats);
+	//clear form
+	function clearForm() {
+		$('#first-name').val('');
+		$('#last-name').val('');
+		$('#phone').val('');
+		$('#email').val('');
+		$('#request').val('');
+	}
+	
+	//dropdown form when seat selected
+	$('#seating-area').click(function(){
+		$('#reservation-form').slideDown('slow');
 
-	//add last name when reserved seat is hovered
-	$('.seat').mouseover(function() {
-		if ( $(this).hasClass('reserved') ) {
-			name = $(this).attr('data-lastname');
-			$(this).children('p').text(name);
-		}
 	})
-
-	//return to reserved when mouse out
-	$('.seat').mouseout(function() {
-		if ( $(this).hasClass('reserved') ) {
-			$(this).children('p').text('Reserved');
-		}
-	})
-
+	
 });
